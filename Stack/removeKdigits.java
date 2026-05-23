fe73228a26193c779a6656bfc9a2a5de77991185
122334444 <145564481+122334444@users.noreply.github.com>
2026-05-23 13:24:24 +0530

    ...

---
+++ Stack/removeKdigits.java
@@ -0,0 +1,42 @@
+package Stack;
+
+import java.util.*;
+
+//LEETCODE 402 -> return the smallest possible integer after removing k digits from num
+
+public class removeKdigits {
+    public static String removeK_digits(String num, int k) {
+        int n = num.length();
+        if (n == k)
+            return "0";
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!s.isEmpty() && k > 0 && (s.peek() - '0') > (num.charAt(i) - '0')) {
+                s.pop();
+                k--;
+            }
+            s.push(num.charAt(i));
+        }
+
+        while (k > 0) {
+            s.pop();
+            k--;
+        }
+
+        StringBuilder res = new StringBuilder();
+        while (!s.isEmpty()) {
+            res.append(s.pop());
+        }
+        res.reverse();
+
+        while (res.length() > 0 && res.charAt(0) == '0') {
+            res.deleteCharAt(0);
+        }
+
+        if (res.length() == 0) {
+            return "0";
+        }
+        return res.toString();
+    }
+}
