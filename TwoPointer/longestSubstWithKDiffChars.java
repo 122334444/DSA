1077332f989ca24e57e2eeba707c89629ce8ca36
122334444 <145564481+122334444@users.noreply.github.com>
2026-05-28 20:15:23 +0530

    ...

--- TwoPointer/longestSubstWithKDiffChars.java
+++ TwoPointer/longestSubstWithKDiffChars.java
@@ -3,7 +3,7 @@ package TwoPointer;
 import java.util.*;
 
 public class longestSubstWithKDiffChars {
-
+    // TC -> O(N^2)
     public static int fxn(String s, int k) {
         int n = s.length();
         Map<Character, Integer> map = new HashMap<>();
@@ -22,10 +22,35 @@ public class longestSubstWithKDiffChars {
         return max;
     }
 
+    // Optimal -> O(N)
+    public static int optimal(String s, int k) {
+        int n = s.length();
+        int l = 0, r = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+
+        while (r < n) {
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
+
+            while (map.size() > k) {
+                map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0) - 1);
+                if (map.get(s.charAt(l)) == 0)
+                    map.remove(s.charAt(l));
+                l++;
+            }
+
+            max = Math.max(max, r - l + 1);
+
+            r++;
+        }
+        return max;
+    }
+
     public static void main(String args[]) {
         String str = "abcacchdfghacvfgdsddsasd";
         int k = 5;
 
-        System.out.println(fxn(str, k));
+        System.out.println(optimal(str, k));
     }
 }
