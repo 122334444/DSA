b446f09d5eb9d48d4011d52bdea12b9a4234e723
122334444 <145564481+122334444@users.noreply.github.com>
2026-06-05 18:41:08 +0530

    ..

--- HashMap/longestConsecutiveSeq.java
+++ HashMap/longestConsecutiveSeq.java
@@ -1,7 +1,41 @@
 package HashMap;
 
+import java.util.*;
+
 public class longestConsecutiveSeq {
     public static void main(String args[]) {
+        int arr[] = { 10, 5, 9, 1, 11, 8, 6, 15, 3, 12, 2 };
 
+        Map<Integer, Boolean> map = new HashMap<>();
+        for (int x : arr) {
+            map.put(x, true);
+        }
+
+        for (int x : arr) {
+            if (map.containsKey(x - 1)) {
+                map.put(x, false);
+            }
+        }
+
+        int msp = 0;
+        int max = 0;
+        for (int x : arr) {
+            if (map.get(x) == true) {
+                int tl = 1;// temporary length
+                int tsp = x;// temp start point
+                while (map.containsKey(tl + tsp)) {
+                    tl++;
+                }
+                if (tl > max) {
+                    max = tl;
+                    msp = tsp;
+                }
+
+            }
+        }
+
+        for (int i = 0; i < max; i++) {
+            System.out.println(msp + i + " ");
+        }
     }
 }
