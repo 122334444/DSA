3a6cf66c795bd16450dddbda59cec88428e1f74e
122334444 <145564481+122334444@users.noreply.github.com>
2026-06-08 17:47:04 +0530

    ...

--- DP/dpOneDimention/zeroOneKnapsack.java
+++ DP/dpOneDimention/zeroOneKnapsack.java
@@ -1,16 +1,21 @@
 package DP.dpOneDimention;
 
+import java.util.*;
+
 public class zeroOneKnapsack {
 
-    public static int knapsack(int wt[], int val[], int w, int idx) {
+    public static int knapsack(int wt[], int val[], int w, int idx, int dp[][]) {
         if (idx < 0 || w == 0) {
             return 0;
         }
+        if (dp[idx][w] != -1)
+            return dp[idx][w];
 
         if (wt[idx] <= w) {
-            return Math.max(val[idx] + knapsack(wt, val, w - wt[idx], idx - 1), knapsack(wt, val, w, idx - 1));
+            return dp[idx][w] = Math.max(val[idx] + knapsack(wt, val, w - wt[idx], idx - 1, dp),
+                    knapsack(wt, val, w, idx - 1, dp));
         } else {
-            return knapsack(wt, val, w, idx - 1);
+            return dp[idx][w] = knapsack(wt, val, w, idx - 1, dp);
         }
 
     }
@@ -20,6 +25,10 @@ public class zeroOneKnapsack {
         int val[] = { 1, 4, 5, 7 };
         int w = 7;
         int n = wt.length;
-        System.out.println(knapsack(wt, val, w, n - 1));
+        int dp[][] = new int[n + 1][w + 1];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println(knapsack(wt, val, w, n - 1, dp));
     }
 }
