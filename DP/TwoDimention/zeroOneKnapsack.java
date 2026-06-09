f817edc3b2211d907473eee3c7e06128278bdc6f
122334444 <145564481+122334444@users.noreply.github.com>
2026-06-09 14:52:26 +0530

    ...

--- DP/TwoDimention/zeroOneKnapsack.java
+++ DP/TwoDimention/zeroOneKnapsack.java
@@ -37,7 +37,7 @@ public class zeroOneKnapsack {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= w; j++) {
                 if (wt[i - 1] <= j) {
-                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][w]);
+                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
