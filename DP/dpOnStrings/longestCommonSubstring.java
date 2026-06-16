b201e60793ab4517c98e45bfebdc7781625a4b02
122334444 <145564481+122334444@users.noreply.github.com>
2026-06-16 19:39:30 +0530

    ..

--- DP/dpOnStrings/longestCommonSubstring.java
+++ DP/dpOnStrings/longestCommonSubstring.java
@@ -1,23 +1,72 @@
 package DP.dpOnStrings;
 
-public class longestCommonSubstring {
+import java.util.*;
 
-    static int maxlen;
+public class LongestCommonSubstring {
 
-    public static int solve(String s1, String s2, int i, int j) {
-        if (i < 0 || j < 0) {
+    // // Reursive
+    // static int maxlen;
+
+    // public static int solve(String s1, String s2, int i, int j) {
+    // if (i < 0 || j < 0) {
+    // return 0;
+    // }
+    // int curr = 0;
+    // if (s1.charAt(i) == s2.charAt(j)) {
+    // curr = 1 + solve(s1, s2, i - 1, j - 1);
+    // maxlen = Math.max(maxlen, curr);
+    // }
+    // // explore all positions
+    // solve(s1, s2, i - 1, j);
+    // solve(s1, s2, i, j - 1);
+
+    // return curr;
+    // }
+
+    // Optimal -> memo
+    private int[][] dp;
+    private int maxLen;
+
+    private int solve(int i, int j, String s1, String s2) {
+        if (i == 0 || j == 0) {
             return 0;
         }
-        int curr = 0;
-        if (s1.charAt(i) == s2.charAt(j)) {
-            curr = 1 + solve(s1, s2, i - 1, j - 1);
-            maxlen = Math.max(maxlen, curr);
-        }
-        // explore all positions
-        solve(s1, s2, i - 1, j);
-        solve(s1, s2, i, j - 1);
 
-        return curr;
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+            dp[i][j] = 1 + solve(i - 1, j - 1, s1, s2);
+            maxLen = Math.max(maxLen, dp[i][j]);
+        } else {
+            dp[i][j] = 0;
+        }
+
+        return dp[i][j];
+    }
+
+    public int longestCommonSubstring(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        dp = new int[n + 1][m + 1];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        maxLen = 0;
+
+        // Evaluate all states because the answer
+        // can end at any position
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                solve(i, j, s1, s2);
+            }
+        }
+
+        return maxLen;
     }
 
     public static void main(String args[]) {
@@ -25,7 +74,7 @@ public class longestCommonSubstring {
         String s2 = "abfce";
         int n = s1.length();
         int m = s2.length();
-        solve(s1, s2, n - 1, m - 1);
-        System.out.println(maxlen);
+        LongestCommonSubstring obj = new LongestCommonSubstring();
+        System.out.println(obj.longestCommonSubstring(s1, s2));
     }
 }
