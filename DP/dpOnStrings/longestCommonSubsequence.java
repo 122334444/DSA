8a8c283f3fa08448fde95f1960a45bb87ef66b40
122334444 <145564481+122334444@users.noreply.github.com>
2026-06-23 21:32:25 +0530

    ...

--- DP/dpOnStrings/longestCommonSubsequence.java
+++ DP/dpOnStrings/longestCommonSubsequence.java
@@ -1,33 +1,36 @@
 package DP.dpOnStrings;
 
+import java.util.*;
+
 public class longestCommonSubsequence {
 
-    // public int longestCommonSubsequence(String x, String y) {
-    // int n=x.length();
-    // int m=y.length();
-    // int dp[][]=new int[n][m];
-    // for(int i=0;i<n;i++){
-    // Arrays.fill(dp[i],-1);
-    // }
-    // return solve(x,y,n-1,m-1,dp);
-    // }
-
-    // private int solve(String x,String y,int i,int j,int dp[][]){
-    // if(i<0 || j<0){//agar0 kare to wo 0 idx per hi stop hojayge
-    // return 0;
-    // }//koi ek bhi str empty hai to max len 0 hi hoga
-
-    // if(dp[i][j]!=-1)return dp[i][j];
-
-    // //Match
-    // if(x.charAt(i)==y.charAt(j)){
-    // return dp[i][j]=1+solve(x,y,i-1,j-1,dp);
-    // }else{//Not Match
-    // return dp[i][j]=Math.max(solve(x,y,i-1,j,dp),solve(x,y,i,j-1,dp));
-    // }
-    // }
-
     public static int longestCommonSubsequence(String x, String y) {
+        int n = x.length();
+        int m = y.length();
+        int dp[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(x, y, n - 1, m - 1, dp);
+    }
+
+    private static int solve(String x, String y, int i, int j, int dp[][]) {
+        if (i < 0 || j < 0) {// agar0 kare to wo 0 idx per hi stop hojayge
+            return 0;
+        } // koi ek bhi str empty hai to max len 0 hi hoga
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        // Match
+        if (x.charAt(i) == y.charAt(j)) {
+            return dp[i][j] = 1 + solve(x, y, i - 1, j - 1, dp);
+        } else {// Not Match
+            return dp[i][j] = Math.max(solve(x, y, i - 1, j, dp), solve(x, y, i, j - 1, dp));
+        }
+    }
+
+    public static int longestCommonSubsequenceTopDown(String x, String y) {
         int n = x.length();
         int m = y.length();
 
