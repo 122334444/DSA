704a8a9c0ea333f047a455ee1923ed8580b16e3b
122334444 <145564481+122334444@users.noreply.github.com>
2026-08-04 15:16:32 +0530

    ...

---
+++ BinarySearch/SquareRootOfNumber.java
@@ -0,0 +1,17 @@
+package BinarySearch;
+
+public class SquareRootOfNumber {
+    public static long sqRoot(long n) {
+        long l = 1, r = n;
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            long val = mid * mid;
+            if (val <= n) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r;
+    }
+}
