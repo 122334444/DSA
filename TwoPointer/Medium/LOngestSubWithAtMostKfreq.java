250bb72e1524eabde04940ceb6db88c694da7aaf
122334444 <145564481+122334444@users.noreply.github.com>
2026-09-25 23:08:59 +0530

    Add comments for clarity in LOngestSubWithAtMostKfreq.java
    Added comments explaining the problem statement and function purpose.

--- TwoPointer/Medium/LOngestSubWithAtMostKfreq.java
+++ TwoPointer/Medium/LOngestSubWithAtMostKfreq.java
@@ -6,6 +6,11 @@ import java.util.Map;
 import java.util.Set;
 
 //Leet Code 2958
+//You are given an integer array nums and an integer k.
+//The frequency of an element x is the number of times it occurs in an array.
+//An array is called good if the frequency of each element in this array is less than or equal to k.
+//Return the length of the longest good subarray of nums.
+
 public class LOngestSubWithAtMostKfreq {
     public int maxSubarrayLength(int[] nums, int k) {
         int n = nums.length;
