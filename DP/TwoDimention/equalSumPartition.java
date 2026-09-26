5572eb497ea8af0d16854c8b2b7b9d859122a32b
122334444 <145564481+122334444@users.noreply.github.com>
2026-09-26 18:23:50 +0530

    ...

--- DP/TwoDimention/equalSumPartition.java
+++ DP/TwoDimention/equalSumPartition.java
@@ -1,5 +1,38 @@
 package DP.TwoDimention;
 
 public class equalSumPartition {
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int x: nums){
+            sum+=x;
+        }
+        if(sum%2==1)return false;
 
+        int k=sum/2;
+        int n=nums.length;
+        Boolean dp[][]=new Boolean[n][k+1];
+         return solve(n-1,nums,k,dp);
+    }
+
+    private boolean solve(int idx,int nums[],int k,Boolean dp[][]){
+        //base case
+        if(idx==0){
+            if(nums[idx]==k){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        if(dp[idx][k]!=null)return dp[idx][k];
+
+        boolean skip=solve(idx-1,nums,k,dp);
+
+        boolean take=false;
+        if(k>=nums[idx]){
+            take=solve(idx-1,nums,k-nums[idx],dp);
+        }
+
+        return dp[idx][k]=(take||skip);
+    }   
 }
